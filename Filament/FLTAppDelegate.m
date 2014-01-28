9d87e4dbeec1fb609ff312ba40e8c8d03759804e
Dan Cutting <dcutting@gmail.com>
2014-01-28 17:58:47 +0000

    Use integration class in sample code.

--- Filament/FLTAppDelegate.m
+++ Filament/FLTAppDelegate.m
@@ -12,29 +12,22 @@
 - (IBAction)clickedIntegrate:(id)sender {
 
     NSString *xctoolPath = @"~/Code/3rdParty/xctool/xctool.sh";
-    NSString *gitPath = @"/usr/local/bin/git";
+    FLTIntegrator *integrator = [[FLTIntegrator alloc] initWithXctoolPath:xctoolPath taskFactory:[NSTaskFactory new] integrationReportGenerator:[FLTIntegrationReportGenerator new]];
     
+    FLTIntegration *integration = [[FLTIntegration alloc] initWithIntegrator:integrator];
+
     NSURL *gitURL = [NSURL URLWithString:@"/Users/dcutting/Code/Tests/BasicApp"];
     NSString *branchName = @"sampleBranch";
     NSString *clonePath = @"/tmp/BasicAppClone";
-    
+
+    NSString *gitPath = @"/usr/local/bin/git";
     FLTRepository *repository = [[FLTRepository alloc] initWithGitPath:gitPath taskFactory:[NSTaskFactory new]];
 
     NSLog(@"Started checkout");
-    
-    [repository checkoutGitURL:gitURL branchName:branchName toPath:clonePath completionHandler:^(FLTIntegratorConfiguration *configuration) {
 
-        NSLog(@"Completed checkout");
-        
-        FLTIntegrator *integrator = [[FLTIntegrator alloc] initWithXctoolPath:xctoolPath taskFactory:[NSTaskFactory new] integrationReportGenerator:[FLTIntegrationReportGenerator new]];
-        
-        NSLog(@"Started integration");
-        
-        [integrator integrateConfiguration:configuration completionHandler:^(FLTIntegrationReport *report) {
-            
-            NSLog(@"Completed integration:");
-            NSLog(@"%@", report);
-        }];
+    [integration integrateGitURL:gitURL branchName:branchName toPath:clonePath repository:repository completionHandler:^(FLTIntegrationReport *report) {
+
+        NSLog(@"Completed integration: %@", report);
     }];
 }
 
