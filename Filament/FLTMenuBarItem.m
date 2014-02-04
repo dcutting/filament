361b20503f8de88872e5faa2941c34af8b180d81
Mike O. Abidakun <mike@mikeosoft.co.uk>
2014-02-04 21:21:21 +0000

    Add FLTMenuBarItem clas.

---
+++ Filament/FLTMenuBarItem.m
@@ -0,0 +1,27 @@
+//  Copyright (c) 2014 Yellowbek Ltd. All rights reserved.
+//
+
+#import "FLTMenuBarItem.h"
+
+@implementation FLTMenuBarItem
+
+
+- (void)awakeFromNib {
+    
+    self.statusItem = [[NSStatusBar systemStatusBar]
+                        statusItemWithLength:NSVariableStatusItemLength];
+    [self.statusItem setHighlightMode:YES];
+    [self.statusItem setTitle:@"Filament"];
+    [self.statusItem setEnabled:YES];
+    [self.statusItem setToolTip:@"Filament CI"];
+    
+    [self.statusItem setAction:@selector(menuBarItemPressed:)];
+    [self.statusItem setTarget:self];
+}
+
+- (IBAction)menuBarItemPressed:(id)sender {
+    NSLog(@"menuBarItemPressed: %@", sender);
+}
+
+
+@end
